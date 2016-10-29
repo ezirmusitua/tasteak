5d9c4eb02955975afe1317cdb3149aa6589d1510
ezirmusitua <jferroal@gmail.com>
2016-10-29 01:05:17 -0400

    Rewrite type constructor basic test

---
+++ test/test-type-constructor.js
@@ -0,0 +1,47 @@
+const mocha = require('mocha');
+const chai = require('chai');
+const constructType = require('../lib/type-constructor').constructSchemaType;
+
+chai.should();
+
+const testVal1 = {
+    sex: () => '',
+    name: {
+        chinese: {
+            first: () => '',
+            last: () => ''
+        },
+        english: {
+            first: () => '',
+            last: () => ''
+        }
+    },
+    interest: [() => ''],
+    age: () => 0,
+};
+
+const testVal2 = {
+    sex: 'string',
+    name: {
+        chinese: {
+            first: 'string',
+            last: 'string'
+        },
+        english: {
+            first: 'string',
+            last: 'string'
+        }
+    },
+    interest: ['string'],
+    age: 'number'
+};
+
+describe('Type constructor', () => {
+    it('should return type structure ', (done) => {
+        const tmp = constructType(testVal1);
+        console.log(tmp);
+
+        tmp.should.deep.equal(testVal2);
+        done();
+    });
+});
